d81daffa1545e3c7ea06bcad31ae4bdc027c73be
Joel Tio <joeltiojinhon@gmail.com>
2016-10-12 22:34:43 +0800

    Bash scripts now exit on error and execute the program automatically. Added fast_build that does not delete build directory.

---
+++ OpenGL_Playground/fast_build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This shell script will do a normal build. If there is no build directory,
+# It will execute clean_build.sh
+
+# Execute a clean build if build directory does not exist.
+
+# Exit if any command fails
+set -e
+# Check if build directory exists
+if [ ! -d "build" ]; then
+    ./clean_build.sh
+    exit
+fi
+
+cd build
+# CMake
+echo "Executing CMake..."
+cmake ..
+# Make
+echo "Executing make..."
+make all
+# Copy executable
+echo "Copying executable to directory"
+cp OpenGL_Playground ..
+# Run executable
+echo "Running..."
+cd ..
+./OpenGL_Playground
+
